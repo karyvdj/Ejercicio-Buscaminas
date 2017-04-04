291b357decf32f51d53bb73f853ba8fe76ba1a5d
OfeliaGarc <ofee.50.og@gmail.com>
2017-04-04 17:34:09 -0500

    Trabajando en boton reinicio

---
+++ funcion.js
@@ -0,0 +1,52 @@
+ // alert("hola")
+ var celdaNumero= document.getElementsByClassName("numero");
+ var celdaColor = document.getElementsByClassName('color');
+ var celdaBomba = document.getElementsByClassName('bomba');
+
+
+ for(i = 0; i < celdaNumero.length; i++){
+   celdaNumero[i].addEventListener("click", clickCeldaNumero)
+ }
+
+ for(i = 0; i < celdaColor.length; i++){
+   celdaColor[i].addEventListener("click", clickCeldaColor)
+ }
+
+ for(i = 0; i < celdaBomba.length; i++){
+   celdaBomba[i].addEventListener("click", clickCeldaBomba)
+ }
+
+ function clickCeldaNumero() {
+   this.innerText = "1";
+ }
+
+ function clickCeldaColor() {
+   var img = document.createElement("img");
+   this.style.backgroundColor = "#4AD0BA";
+ }
+
+ function clickCeldaBomba() {
+   var img = document.createElement("img");
+   img.src = "img/bombs.png";
+   img.setAttribute( "width", "45px");
+   img.setAttribute( "height", "45px");
+   this.appendChild(img);
+
+   inavilitarCeldas();
+   alert ("PUM!!!\nGAME OVER")
+ }
+
+ function inavilitarCeldas(){
+   for(i = 0; i < celdaNumero.length; i++){
+     celdaNumero[i].removeEventListener("click", clickCeldaNumero);
+   }
+
+   for(i = 0; i < celdaBomba.length; i++){
+     celdaBomba[i].removeEventListener("click", clickCeldaBomba)
+   }
+
+   for(i = 0; i < celdaColor.length; i++){
+     celdaColor[i].removeEventListener("click", clickCeldaColor)
+   }
+
+ }
